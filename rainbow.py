112b781fc47032ff58f4168d7ebb51e1c27c7b12
pbednarczyk <ibednar88@gmail.com>
2022-09-30 22:55:22 +0200

    wswsws

--- rainbow.py
+++ rainbow.py
@@ -13,6 +13,8 @@ strip = Adafruit_NeoPixel(LEDCOUNT, GPIOPIN, FREQ, DMA, INVERT, BRIGHTNESS)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
+current_color = RgbColor(0, 0, 0)
+
 class RgbColor:
     def __init__(self, red, green, blue):
         self.red = red
@@ -22,29 +24,29 @@ class RgbColor:
     def __eq__(self, other):
         return self.red == other.red and self.green == other.green and self.blue == other.blue
 
-def set_color(self, color, wait_ms=0):
+def set_color(color, wait_ms=0):
         """Wipe color across display a pixel at a time."""
-        if self._current_color == color:
+        if current_color == color:
             # No change
             return
         logging.info('Changing LED strip color to (%u,%u,%u)' % (color.red, color.green, color.blue))
-        for i in range(self._strip.numPixels()):
-            self._strip.setPixelColor(i, Color(color.green, color.red, color.blue))
-            self._strip.show()
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(color.green, color.red, color.blue))
+            strip.show()
             # time.sleep(wait_ms/1000.0)
-        self._current_color = color
+        current_color = color
 
 
 while True:
     # First LED white
-    set_color(strip, RgbColor(255,255,255))
+    set_color(RgbColor(255,255,255))
     time.sleep(1)
     # LEDs Red
-    set_color(strip, RgbColor(255,0,0))
+    set_color(RgbColor(255,0,0))
     time.sleep(1)
     # LEDs Green
-    set_color(strip, RgbColor(0,255,0))
+    set_color(RgbColor(0,255,0))
     time.sleep(1)
     # LEDs Blue
-    set_color(strip, RgbColor(0,0,255))
+    set_color(RgbColor(0,0,255))
     time.sleep(1)
