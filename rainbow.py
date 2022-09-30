f77199d0ad099af86d368c034b3c034cc1131261
pbednarczyk <ibednar88@gmail.com>
2022-09-30 22:39:59 +0200

    change all

--- rainbow.py
+++ rainbow.py
@@ -1,96 +1,33 @@
-
-# Simple test for NeoPixels on Raspberry Pi
+#!/usr/bin/python3
 from neopixel import *
 import time
-import numpy as np
 
-
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = 18
-
-# The number of NeoPixels
-num_pixels = 44
-
-LED_FREQ_HZ = 800000
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-# ORDER = neopixel.RGB
-# 
-# pixels = neopixel.NeoPixel(
-# # pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER
-# )
-
-class RgbColor:
-    def __init__(self, red, green, blue):
-        self.red = red
-        self.green = green
-        self.blue = blue
-
-    def __eq__(self, other):
-        return self.red == other.red and self.green == other.green and self.blue == other.blue
-
-strip = Adafruit_NeoPixel(num_pixels, num_pixels, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-
-
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-
-    return RgbColor(r, g, b)
-
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            color = wheel(pixel_index & 255)
-            strip.setPixelColor(i, Color(color.red, color.green, color.blue))
-            strip.show()
-        time.sleep(wait)
-
-
+LEDCOUNT = 44 # Number of LEDs
+GPIOPIN = 18
+FREQ = 800000
+DMA = 5
+INVERT = True # Invert required when using inverting buffer
+BRIGHTNESS = 255
+strip = Adafruit_NeoPixel(LEDCOUNT, GPIOPIN, FREQ, DMA, INVERT, BRIGHTNESS)
+# Intialize the library (must be called once before other functions).
+strip.begin()
 while True:
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((255, 0, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
-
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 255, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
-
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 0, 255, 0))
-    # pixels.show()
-    # time.sleep(1)
-
-    rainbow_cycle(0.01)  # rainbow cycle with 1ms delay per step
+    # First LED white
+    strip.setPixelColor(0, Color(255,255,255))
+    strip.setPixelColor(1, Color(0,0,0))
+    strip.show()
+    time.sleep(0.5)
+    # Second LED white
+    strip.setPixelColor(0, Color(0,0,0))
+    strip.setPixelColor(1, Color(255,255,255))
+    strip.show()
+    time.sleep(1)
+    # LEDs Red
+    strip.setPixelColor(0, Color(255,0,0))
+    strip.setPixelColor(1, Color(255,0,0))
+    strip.show()
+    time.sleep(0.5)
+    # LEDs Green
+    strip.setPixelColor(0, Color(0,255,0))
+    strip.setPixelColor(1, Color(0,255,0))
+    strip.show()
